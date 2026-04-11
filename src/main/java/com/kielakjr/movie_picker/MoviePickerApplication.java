c7ee285193892d6fe4b9a407149eb458218b8e86
mkielak <kielakowsky17@gmail.com>
2026-04-11 10:36:42 +0200

    Initial commit

---
+++ src/main/java/com/kielakjr/movie_picker/MoviePickerApplication.java
@@ -0,0 +1,13 @@
+package com.kielakjr.movie_picker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviePickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kielakjr/movie_picker/MoviePickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kielakjr.movie_picker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviePickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
