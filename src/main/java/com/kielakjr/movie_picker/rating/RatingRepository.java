582e07555bb9eb8b2a50b6be62ad82433634b162
mkielak <kielakowsky17@gmail.com>
2026-04-11 18:54:14 +0200

    Added repos for base

---
+++ src/main/java/com/kielakjr/movie_picker/rating/RatingRepository.java
@@ -0,0 +1,10 @@
+package com.kielakjr.movie_picker.rating;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+    List<Rating> findByUserId(Long userId);
+}
